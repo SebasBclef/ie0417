3aabf784cfbd43ea7abb08d2ddb0504be0fd93be
Sebastian Bonilla <sebastian.bonillavega@ucr.ac.cr>
2025-04-11 15:10:02 -0600

    Agregada la documentación generada por Doxygen

---
+++ docs/html/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['gameover_0',['gameOver',['../main_8cpp.html#a1fb9f73c50443018c691b0eaf1066004',1,'main.cpp']]],
+  ['gotoxy_1',['gotoxy',['../main_8cpp.html#ae824443b3f661414ba1f2718e17fe97d',1,'main.cpp']]]
+];
---
+++ docs/html/html/search/variables_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['ballposx_0',['ballPosX',['../main_8cpp.html#a5a689a1c95e6644c023df0a27949dd8d',1,'main.cpp']]],
+  ['ballposy_1',['ballPosY',['../main_8cpp.html#a8d525bf22f597ba7422c868283200350',1,'main.cpp']]],
+  ['ballreset_2',['ballReset',['../main_8cpp.html#a77481b3330f468967ae3169ba6a3e9bd',1,'main.cpp']]],
+  ['ballvelocityx_3',['ballVelocityX',['../main_8cpp.html#a4a700e02d53b2ccdaf66ae3f69658bd2',1,'main.cpp']]],
+  ['ballvelocityy_4',['ballVelocityY',['../main_8cpp.html#aac5fcc1963afbe08a9bbbbdb2dab1ccc',1,'main.cpp']]]
+];
